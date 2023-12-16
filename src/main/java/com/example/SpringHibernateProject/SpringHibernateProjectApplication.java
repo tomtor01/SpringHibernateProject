0368c332e8d149b6f0d0b643ca93f67850d325ba
Tomasz Torchalski <tomtor@st.amu.edu.pl>
2023-12-16 12:14:03 +0100

    SpringBoot skeleton added

---
+++ src/main/java/com/example/SpringHibernateProject/SpringHibernateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringHibernateProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringHibernateProject/SpringHibernateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringHibernateProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
